1c5ac8541031810aafd48d11f83d33a1d26be79a
ronis <ronis.tzol@gmail.com>
2024-09-20 18:14:38 +0100

    disable server side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Desktop from '@/features/desktop/Desktop';
+import dynamic from 'next/dynamic';
+
+const DynamicComponent = dynamic(() => import('@/features/desktop/Desktop'), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <main>
-      <Desktop />
+      <DynamicComponent />
     </main>
   );
 }
