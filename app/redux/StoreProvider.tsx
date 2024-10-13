2d4fac4789c6a87c4fa4b5291defc1769d5611e5
ronis <ronis.tzol@gmail.com>
2024-10-13 22:42:59 +0100

    install redux persist

--- app/redux/StoreProvider.tsx
+++ app/redux/StoreProvider.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import React from 'react';
 
 const StoreProvider = ({ children }: any) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default StoreProvider;
--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,22 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import appSlice from './app/appSlice';
 import editorSlice from './editor/editorSlice';
 import minesweeperSlice from './minesweeper/minesweeperSlice';
 import contextSlice from './contextMenu/contextSlice';
+import sessionStorage from 'redux-persist/lib/storage/session';
+
+const persistConfig = {
+  key: 'root',
+  storage: sessionStorage,
+};
+
+const rootReducer = combineReducers({
+  app: appSlice,
+  editor: editorSlice,
+  minesweeper: minesweeperSlice,
+  context: contextSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    app: appSlice,
-    editor: editorSlice,
-    minesweeper: minesweeperSlice,
-    context: contextSlice,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({
       serializableCheck: false,
     });
   },
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
